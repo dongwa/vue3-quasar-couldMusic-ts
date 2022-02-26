f00fd493ebdcecf0ebfb6a33c60f24ff136a5708
dongwa <783137663@qq.com>
2022-02-26 23:21:31 +0800

    feat: 新增搜索单曲页面

---
+++ src/pages/search/Song.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>单曲:{{ word }}</div>
+</template>
+<script lang='ts' setup>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+
+const word = ref(route.query.word)
+</script>
+<style lang='scss'>
+</style>
