a73e51812913bb3909592673149bf69530c77794
donglin <leidonglin@xiaomi.com>
2022-04-22 11:34:05 +0800

    refactor: 左边抽屉在断点小时自动隐藏

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,16 +7,7 @@
           <q-icon size="lg" name="track_changes" />网抑云音乐
         </q-toolbar-title>
         <!-- 前进后退按钮 -->
-        <q-btn
-          round
-          size="sm"
-          class="q-mx-sm"
-          flat
-          color="white"
-          glossy
-          icon="keyboard_arrow_left"
-          @click="goBack"
-        />
+        <q-btn round size="sm" class="q-mx-sm" flat color="white" glossy icon="keyboard_arrow_left" @click="goBack" />
         <q-btn round size="sm" flat color="white" glossy icon="keyboard_arrow_right" />
         <!-- 搜索框 -->
         <Search />
@@ -24,12 +15,8 @@
         <q-btn round size="md" flat color="grey-1" class="q-ml-md bg-red-7" icon="keyboard_voice" />
         <!-- 头像登录等 -->
         <q-space />
-        <q-avatar
-          v-if="!auth.isLogin"
-          size="70px"
-          icon="account_circle"
-          @click="layout.toggleLoginForm(true)"
-        ></q-avatar>
+        <q-avatar v-if="!auth.isLogin" size="70px" icon="account_circle" @click="layout.toggleLoginForm(true)">
+        </q-avatar>
         <q-avatar v-else size="70px" :icon="getAvatar" />
         <span class="cursor-pointer btn-login">
           {{ auth.isLogin ? auth.userInfo.nickname : '未登录' }}
@@ -47,7 +34,7 @@
       </q-toolbar>
     </q-header>
     <!-- 左边抽屉 -->
-    <q-drawer v-model="layout.leftDrawerOpen" side="left" bordered behavior="desktop" :width="230">
+    <q-drawer v-model="layout.leftDrawerOpen" side="left" bordered show-if-above :width="230">
       <side-bar />
     </q-drawer>
     <!-- 右边抽屉，只在点击当前歌单按钮时展开 -->
@@ -93,10 +80,12 @@ const getAvatar = computed(() => `img:${auth.userInfo.avatarUrl}`);
   display: flex;
   height: $header-height;
 }
+
 .btn-login {
   font-size: 11px;
   color: $header-text-color;
 }
+
 .btn-login:hover {
   color: white;
 }
